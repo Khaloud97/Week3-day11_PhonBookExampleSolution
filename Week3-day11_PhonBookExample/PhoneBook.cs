e7d491391e09785b8c5054f9625d0dea933c31e5
Khaloud <hamedkhaloud71@gmail.com>
2023-11-30 13:54:11 +0400

    Add project files.

---
+++ Week3-day11_PhonBookExample/PhoneBook.cs
@@ -0,0 +1,119 @@
+﻿
+using System.Drawing;
+
+namespace Week3_day11_PhonBookExample
+{
+    internal class PhoneBook
+    {
+
+        //is attribute by default is private Member
+        public static int cnt ;
+        string[] Name;
+        long[] Number;
+        int size;
+
+        public PhoneBook(int size)
+        {
+            this.size = size;
+            this.Name = new string[size];
+            this.Number = new long[size];
+        }
+
+        public  int GetSize()
+        {
+            return this.size;
+        }
+
+        //this function use to add person with name and number
+        public void addPerson( string name, long number)
+        {
+            
+            Name[cnt] = name;
+            Number[cnt] = number;
+            cnt++;
+        }
+
+        public void GetAllNum()
+        {
+            for (int i = 0; i < this.Number.Length; i++)
+            {
+                if (this.Name[i] != null)//why i write name not number becouse nuber by defult is 0 but name is 1
+                    Console.WriteLine($"In Index {i} :Name:{this.Name[i]} , Phone Number = {this.Number[i]}");
+            }
+        }
+
+        //-----------------------get and set  number  by using indexer property ------------------------ 
+        // this ==>   Myphone[] object
+        public long this[string name ,bool flag =false]
+        {
+            get {
+                for (int i = 0; i < this.size; i++)
+                {
+                    if (Name[i] == name)
+                    {
+                        return this.Number[i];
+                    }
+                }
+                return -1;
+            }
+            set
+            {
+                if (!flag)
+                {
+                    ////if you want To add value write this code :
+                    Name[cnt] = name;
+                    Number[cnt] = value;
+                    cnt += 1;
+                }
+                else
+                {
+                    //update value :
+                    for (int i = 0; i < this.size; i++)
+                    {
+                        if (Name[i] == name)
+                        {
+                            this.Number[i] = value;
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+        //-----------------------get number ------------------------
+
+        public long GetNumber( string name)
+        {
+            for (int i = 0; i < this.size; i++)
+            {
+                if (Name[i] == name) {
+                    return this.Number[i];
+                }    
+            }
+            return -1;
+
+
+        }
+
+        public void EditNumber(string name, long number)
+        {
+            for (int i = 0; i < this.size; i++)
+            {
+                if (Name[i] == name)
+                {
+                     this.Number[i]=number;
+                    Console.WriteLine($"{Name[i]} is updated with New number {Number[i]}");
+                }
+            }
+            
+
+
+        }
+
+        //set the name of number
+
+
+
+    }
+}
---
+++ Week3-day11_PhonBookExample/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Week3_day11_PhonBookExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Phone Number System");
+
+            PhoneBook Myphone = new PhoneBook(5);
+
+            Myphone.addPerson("Khaloud", 987654);
+            Myphone.addPerson("Sara", 998877);
+            Myphone.addPerson("Reem", 981254);
+
+            Myphone.GetAllNum();
+            Console.WriteLine("----------------------------\n");
+
+            Console.Write("The number of Khaloud is :");
+            Console.WriteLine(Myphone.GetNumber("Khaloud"));//to get phone number of this name
+            Myphone.EditNumber("Khaloud", 9955116);
+
+            Console.Write("The new number is :");
+            Console.WriteLine(Myphone.GetNumber("Khaloud"));
+
+
+            //--------------------------------------------------------------------------------------------------------------
+
+            // indexer propry
+            //get phone number of this name
+            Console.WriteLine("------------indexer propry----------------\n"); 
+
+            Myphone.addPerson("Mona", 9112254); //add new person 
+
+            Console.Write("The old number of Mona is :");
+            Console.WriteLine(Myphone["Mona"]);//to get phone number of this name by indexer
+            Myphone ["Mona"]= 9335116; //edit | update 
+            Myphone.GetAllNum();
+
+
+            //--------------------------------------------------------------------------------------------------------------
+
+
+
+            Console.WriteLine("----------------------------\n");
+            Console.WriteLine("Total count number of phone:");
+            Console.WriteLine(PhoneBook.cnt);// to find the count of phone you have 
+
+
+        }
+    }
+}
